190f13c6ed3875814d0e33b7c50b8cc5d37b0dc9
Miguel Diaz <lukejason05@gmail.com>
2025-09-13 04:04:47 -0400

    update calendar

--- src/components/CalComEmbed.tsx
+++ src/components/CalComEmbed.tsx
@@ -1,23 +1,13 @@
-import { getCalApi } from "@calcom/embed-react";
-import { useEffect } from "react";
-
 const CalComEmbed = () => {
-  useEffect(() => {
-    (async function () {
-      const cal = await getCalApi({"namespace":"45-strategy-call"});
-      cal("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
-    })();
-  }, []);
-
   return (
-    <button 
-      data-cal-namespace="45-strategy-call"
-      data-cal-link="migueldiaz/45-strategy-call"
-      data-cal-config='{"layout":"month_view"}'
-      className="w-full h-full min-h-[700px] bg-gradient-to-r from-blue-500 to-purple-600 text-white font-bold py-4 px-8 rounded-xl hover:shadow-lg transition-all duration-300 flex items-center justify-center text-xl"
-    >
-      Click to Open Calendar
-    </button>
+    <iframe
+      src="https://cal.com/migueldiaz/45-strategy-call?embed=true"
+      width="100%"
+      height="700"
+      frameBorder="0"
+      title="Book a Consultation"
+      className="rounded-xl"
+    ></iframe>
   );
 };
 
